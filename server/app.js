e2b2734f760b4dfd4cae4b9820c897cd4222136d
aswathi-48 <aswathik482@gmail.com>
2024-06-14 18:40:35 +0530

    app.js setting

--- server/app.js
+++ server/app.js
@@ -1,1 +1,20 @@
 import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import { errorHandler, notFound } from './middlewares/errorMiddleare'
+
+const port = process.env.PORT || 7000
+const app = express()
+dotenv.config()
+
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended : true}))
+
+
+
+app.use(notFound)
+app.use(errorHandler)
+
+app.listen(port, () => console.log(`server running om port ${port}`));
